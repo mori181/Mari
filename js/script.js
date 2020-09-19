d0483d6f821a037828e55d1e6ebed0d461e86b2c
Mari <example@gmail.com>
2020-09-19 15:05:22 +0300

    first commit

--- js/script.js
+++ js/script.js
@@ -1,24 +1,23 @@
-"use strict";
-
-console.log('arr' + "-object");
-console.log(4 + "5");
-
-let incr = 10,
-    decr = 10;
-
-//++incr;
-//--decr;
-
-console.log(++incr);
-console.log(--decr);
-
-console.log(5%2);
-
-console.log(2 + 2 * 2 !== 8);
 
 
 
-const isChecked = false,
-      isClose = false;
 
-console.log(isChecked || !isClose);
+const numberOfFilms = +prompt('сколько фильмов вы уже посмотрели?', '');
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const a = prompt('Один из последних просмотренных фильмов?', ''),
+      b = prompt('На сколько оцените его?', ''),
+      c = prompt('Один из последних просмотренных фильмов?', ''),
+      d = prompt('На сколько оцените его?', '');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
