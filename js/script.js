80503b4d5ad778914ced54d72ebd559050d5f509
Mari <example@gmail.com>
2020-09-20 14:09:43 +0300

    new

--- js/script.js
+++ js/script.js
@@ -1,23 +1,39 @@
+"use strict"
+
+if (4 == 9) {
+    console.log('Ok!');
+} else{
+    console.log('error');
+}
 
 
 
+//if(num < 49){
+  //  console.log('error');
+//} else if(num > 100){
+//    console.log('Max');
+//} else{
+  //  console.log('Ok!');
+//}
 
-const numberOfFilms = +prompt('сколько фильмов вы уже посмотрели?', '');
 
-const personalMovieDB = {
-    count: numberOfFilms,
-    movies: {},
-    actors: {},
-    genres: [],
-    privat: false
-};
 
-const a = prompt('Один из последних просмотренных фильмов?', ''),
-      b = prompt('На сколько оцените его?', ''),
-      c = prompt('Один из последних просмотренных фильмов?', ''),
-      d = prompt('На сколько оцените его?', '');
+//(num === 50) ? console.log('Ok!') : console.log('error');
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+//4 + 4
 
-console.log(personalMovieDB);
+const num = '50';
+switch (num) {
+    case '49':
+        console.log('Неверно');
+        break;
+    case '100':
+        console.log('Неверно');
+        break;
+    case '50':
+        console.log('В точку!');
+        break;
+    default:
+        console.log('Не в этот раз');
+        break;
+}
